d3cbd52ed89c0f20378fce1eab7d336f40d08df5
Munteanu Andrei-Stefan <andreistefan@housatonic.microsoftonline.com>
2019-01-30 20:48:36 +0200

    Remove non spa front

--- Tamagotchi/Frontend/Pages/js/loadpage.js
+++
@@ -1,8 +0,0 @@
-    window.addEventListener("load", function() {
-    var load_screen = document.getElementById("page_loader");
-    document.body.removeChild(load_screen);
-            
-    document.getElementsByClassName("main-container")[0].style.display = "block";
-    document.getElementsByClassName("main-container")[1].style.display = "block";
-    document.getElementsByClassName("header-container")[0].style.display = "block";
-});
